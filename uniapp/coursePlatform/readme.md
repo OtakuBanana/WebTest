e5b5437e9b1cc71c392ab0b028407c6f49f7ece0
纸鸢 <1725837470@qq.com>
2022-08-03 16:44:46 +0800

    Add files via upload

---
+++ uniapp/coursePlatform/readme.md
@@ -0,0 +1,33 @@
+##基础知识储备
+#HTML、CSS、JavaScript(ES6)、Vue
+
+##HTML(结构)
+#View容器
+#Text文本
+#swiper
+#image
+#video
+#button
+#input
+
+##CSS样式
+#属性
+	-字体
+	-大小
+	-布局：float
+#选择器：class类选择器 id类选择器
+#取值与单位
+	-px
+	-color
+	-rpx：自动根据屏幕大小适配比例
+#盒子模型
+	-margin：外边距
+	-padding：内边距
+	border：边框
+#弹性盒子模型
+
+##JavaScript
+#页面的动态绑定数据
+#事件处理
+#生命周期函数
+#网络请求
---
+++ uniapp/coursePlatform/uni_modules/uni-nav-bar/navbar/navbar.vue
@@ -0,0 +1,184 @@
+<template>
+	<view class="example-body">
+		<uni-nav-bar statusBar shadow :fixed="false" color="#333333" backgroundColor="#ffffff" right-icon="scan">
+			<view class="input-view">
+				<uni-icons style="float: left;" type="search" size="22" color="#666666" />
+				<input confirm-type="search" type="text" placeholder="请输入关键字" />
+			</view>
+		</uni-nav-bar>
+	</view>
+</template>
+
+<script>
+	import uniNavBar from '@/uni_modules/uni-nav-bar/components/uni-nav-bar/uni-nav-bar.vue'
+	import icons from "@/uni_modules/uni-icons/components/uni-icons/uni-icons.vue"
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	$nav-height: 44px;
+
+	.input-view {
+		margin-top: 20rpx;
+		width: 500rpx;
+	}
+
+	.uni-navbar {
+		// box-sizing: border-box;
+	}
+
+	.uni-nav-bar-text {
+		/* #ifdef APP-PLUS */
+		font-size: 34rpx;
+		/* #endif */
+		/* #ifndef APP-PLUS */
+		font-size: 14px;
+		/* #endif */
+	}
+
+	.uni-nav-bar-right-text {
+		font-size: 12px;
+	}
+
+	.uni-navbar__content {
+		position: relative;
+		// background-color: #fff;
+		// box-sizing: border-box;
+		background-color: transparent;
+	}
+
+	.uni-navbar__content_view {
+		// box-sizing: border-box;
+	}
+
+	.uni-navbar-btn-text {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		line-height: 12px;
+	}
+
+	.uni-navbar__header {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		padding: 0 10px;
+		flex-direction: row;
+		height: $nav-height;
+		font-size: 12px;
+	}
+
+	.uni-navbar__header-btns {
+		/* #ifndef APP-NVUE */
+		overflow: hidden;
+		display: flex;
+		/* #endif */
+		flex-wrap: nowrap;
+		flex-direction: row;
+		width: 120rpx;
+		// padding: 0 6px;
+		justify-content: center;
+		align-items: center;
+		/* #ifdef H5 */
+		cursor: pointer;
+		/* #endif */
+	}
+
+	.uni-navbar__header-btns-left {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		width: 120rpx;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	.uni-navbar__header-btns-right {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		// width: 150rpx;
+		// padding-right: 30rpx;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	.uni-navbar__header-container {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex: 1;
+		padding: 0 10px;
+		overflow: hidden;
+	}
+
+	.uni-navbar__header-container-inner {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex: 1;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		font-size: 12px;
+		overflow: hidden;
+		// box-sizing: border-box;
+	}
+
+
+	.uni-navbar__placeholder-view {
+		height: $nav-height;
+	}
+
+	.uni-navbar--fixed {
+		position: fixed;
+		z-index: 998;
+		/* #ifdef H5 */
+		left: var(--window-left);
+		right: var(--window-right);
+		/* #endif */
+		/* #ifndef H5 */
+		left: 0;
+		right: 0;
+		/* #endif */
+
+	}
+
+	.uni-navbar--shadow {
+		box-shadow: 0 1px 6px #ccc;
+	}
+
+	.uni-navbar--border {
+		border-bottom-width: 1rpx;
+		border-bottom-style: solid;
+		border-bottom-color: #eee;
+	}
+
+	.uni-ellipsis-1 {
+		overflow: hidden;
+		/* #ifndef APP-NVUE */
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		/* #endif */
+		/* #ifdef APP-NVUE */
+		lines: 1;
+		text-overflow: ellipsis;
+		/* #endif */
+	}
+
+	// 暗主题配置
+	.uni-dark {}
+</style>
